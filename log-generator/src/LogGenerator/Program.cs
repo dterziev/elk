472019536b2b10fc9ac3238f5bfb3736ce3bd6f3
Dimo Terziev <dimo.terziev@gmail.com>
2017-05-23 00:02:57 +0100

    log generator.

---
+++ log-generator/src/LogGenerator/Program.cs
@@ -0,0 +1,116 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LogGenerator
+{
+    class Program
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
+
+        static int Main(string[] args)
+        {
+            Console.WriteLine(AppContext.BaseDirectory);
+#if NETCOREAPP1_1
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+#else
+            AppDomain.UnhandledException += (s, e) => 
+            {
+                Log.Fatal("Unhandled error occured. Terminating the process. Data loss might occur.", e.ExceptionObject);
+                Environment.ExitCode = -1;
+            });
+#endif
+            var cts = new CancellationTokenSource();
+            var exitRequestedTask = Task.Delay(int.MaxValue, cts.Token);
+
+            Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            var workerTask = Task.Run(
+                () => LogStuff(cts.Token),
+                cts.Token);
+
+
+            Console.WriteLine("Press Ctrl+C to exit...");
+
+            try
+            {
+                Task.WaitAny(exitRequestedTask, workerTask);
+
+                if (!workerTask.Wait(TimeSpan.FromSeconds(5)))
+                {
+                    Log.Fatal("Shutdown timeout expired. Terminating the process. Data loss might occur.");
+                    return -1;
+                }
+            }
+            catch (AggregateException ex)
+            {
+                try
+                {
+                    ex.Flatten().Handle(exception => exception is TaskCanceledException);
+                }
+                catch (AggregateException ex2)
+                {
+                    Log.Fatal("Unexpected error occured. Terminating the process.", ex2);
+                    return -1;
+                }
+            }
+
+            Log.Info("Process terminated successfully.");
+            return 0;
+        }
+
+        private static void LogStuff(CancellationToken cancellationToken)
+        {
+            var rnd = new Random();
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                if(rnd.Next(100) > 90)
+                {
+                    LogException();
+                }
+                else
+                {
+                    LogEvent(
+                        Guid.NewGuid(), 
+                        success: (rnd.Next(10)) > 1,
+                        durationInMilliseconds: 50 + rnd.Next(200));
+                }
+
+                Task.Delay(500, cancellationToken).Wait();
+            }
+        }
+
+        private static void LogEvent(Guid correlationId, bool success, int durationInMilliseconds)
+        {
+            if (success)
+            {
+                Log.Info($"Operation completed successfully. {{ duration:{durationInMilliseconds}, correlationId:{correlationId:N} }}");
+            }
+            else
+            {
+                Log.Warn($"Operation failed. {{ duration:{durationInMilliseconds}, correlationId:{correlationId:N} }}");
+            }
+        }
+
+        private static void LogException()
+        {
+            try
+            {
+                throw new Exception("Some random exception.");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
+        }
+    }
+}
